63de8417de8a040e0071c85653e09062e91cf7bb
reddy-epk <reddyepk@gmail.com>
2024-06-09 19:51:11 +0000

    cardsupdate

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,108 @@
-const Projects = () => {
-  return <h1>HI</h1>
+import {Component} from 'react'
+import {FaPodcast} from 'react-icons/fa6'
+
+import Header from '../HeaderL'
+import './index.css'
+
+class Projects extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className='main-container'>
+          <div className='projects-container'>
+            <h1>Projects</h1>
+            <button type='button' id='podcasty-button'>
+              <FaPodcast className='podcasty-icon' />
+              <span>Create a New PodCast</span>
+            </button>
+          </div>
+          <div className='projects-list-container'>
+            <div class='card'>
+              <div class='card-image'></div>
+              <div class='card-content'>
+                <div>
+                  <h3 class='card-title'>Sample Project</h3>
+                  <p class='card-text'>4 Episodes</p>
+                </div>
+                <p> Last edited a week ago</p>
+              </div>
+            </div>
+            <div class='card'>
+              <div class='card-image'></div>
+              <div class='card-content'>
+                <div>
+                  <h3 class='card-title'>Sample Project</h3>
+                  <p class='card-text'>4 Episodes</p>
+                </div>
+                <p> Last edited a week ago</p>
+              </div>
+            </div>
+            <div class='card'>
+              <div class='card-image'></div>
+              <div class='card-content'>
+                <div>
+                  <h3 class='card-title'>Sample Project</h3>
+                  <p class='card-text'>4 Episodes</p>
+                </div>
+                <p> Last edited a week ago</p>
+              </div>
+            </div>
+            <div class='card'>
+              <div class='card-image'></div>
+              <div class='card-content'>
+                <div>
+                  <h3 class='card-title'>Sample Project</h3>
+                  <p class='card-text'>4 Episodes</p>
+                </div>
+                <p> Last edited a week ago</p>
+              </div>
+            </div>
+            <div class='card'>
+              <div class='card-image'></div>
+              <div class='card-content'>
+                <div>
+                  <h3 class='card-title'>Sample Project</h3>
+                  <p class='card-text'>4 Episodes</p>
+                </div>
+                <p> Last edited a week ago</p>
+              </div>
+            </div>
+            <div class='card'>
+              <div class='card-image'></div>
+              <div class='card-content'>
+                <div>
+                  <h3 class='card-title'>Sample Project</h3>
+                  <p class='card-text'>4 Episodes</p>
+                </div>
+                <p> Last edited a week ago</p>
+              </div>
+            </div>
+            <div class='card'>
+              <div class='card-image'></div>
+              <div class='card-content'>
+                <div>
+                  <h3 class='card-title'>Sample Project</h3>
+                  <p class='card-text'>4 Episodes</p>
+                </div>
+                <p> Last edited a week ago</p>
+              </div>
+            </div>
+            <div class='card'>
+              <div class='card-image'></div>
+              <div class='card-content'>
+                <div>
+                  <h3 class='card-title'>Sample Project</h3>
+                  <p class='card-text'>4 Episodes</p>
+                </div>
+                <p> Last edited a week ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Projects
